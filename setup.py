04778c45bf8bbfe647545bb1c219a367ba603091
yuyansong <yuyansong@baidu.com>
2017-04-21 14:08:27 +0800

    fix 创建父目录 & 监听挂掉

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FastSync',
-    version='0.1.0.1',
+    version='0.1.0.2',
     packages=find_packages(),
     install_requires=[
         'requests',
